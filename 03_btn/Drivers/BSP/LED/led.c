4fa13647aa8a02e08263341ddf3f8e7182b12c67
jie <a2223889679@gmail.com>
2025-06-18 00:58:21 +0800

    03_btn

---
+++ 03_btn/Drivers/BSP/LED/led.c
@@ -0,0 +1,9 @@
+/*
+ * led.c
+ *
+ *  Created on: Jun 15, 2025
+ *      Author: jie
+ */
+
+
+#include "led.h"
---
+++ 03_btn/Drivers/BSP/LED/led.h
@@ -0,0 +1,24 @@
+/*
+ * LED.h
+ *
+ *  Created on: Jun 15, 2025
+ *      Author: jie
+ */
+
+#ifndef BSP_LED_LED_H_
+#define BSP_LED_LED_H_
+
+# include "main.h"
+# include "gpio.h"
+
+# define LED0(x) x ? HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET) \
+            	 : HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET)
+
+# define LED1(x) x ? HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET) \
+            	 : HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET)
+
+
+# define LED0_TOGGLE()  HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin)
+# define LED1_TOGGLE()  HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin)
+
+#endif /* BSP_LED_LED_H_ */
